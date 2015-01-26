5b0f7325150101bf09a1f2bf0036400bd7fde37d
Adam Krebs <amk528@cs.nyu.edu>
2015-01-25 22:34:02 -0500

    rename fn.wrapped to fn._wrapped and only export Backbone.Attributes on the global. We should avoid mutating the Backbone obj unecessarily

--- backbone.attributes.js
+++ backbone.attributes.js
@@ -9,9 +9,10 @@
 (function (factory) {
   if (typeof define === 'function' && define.amd) { define(['underscore', 'backbone', 'exports'], factory);
   } else if (typeof exports === 'object') { factory(require('underscore'), require('backbone'), exports);
-  } else { factory(_, Backbone, {}); }
+  } else { factory(_, Backbone, (Backbone.Attributes = {})); }
 }(function (_, Backbone, Attributes) {
-  _.extend(Backbone.Attributes = Attributes, Backbone.Events);
+  _.extend(Attributes, Backbone.Events);
+
   var modelMethods = ['get', 'set', 'unset', 'clear', '_validate', 'validate', 'isValid', 'has', 'changed', 'hasChanged', 'changedAttributes', 'previous', 'previousAttributes'];
   var wrappedMethods = ['get', 'set', 'clear', 'changedAttributes'];
 
@@ -21,12 +22,12 @@
     var fn = Model[method];
     var wrapper = function() {
       _.defaults(this.attributes || (this.attributes = {}), _.result(this, 'defaults'));
-      _.each(wrappedMethods, function(m) { if (this[m].wrapped) this[m] = Model[m]; }, this);
+      _.each(wrappedMethods, function(m) { if (this[m]._wrapped) this[m] = Model[m]; }, this);
       this.setAttribute = this.setAttributes = Model.set;
       this.getAttribute = Model.get;
       return fn.apply(this, arguments);
     };
-    wrapper.wrapped = true;
+    wrapper._wrapped = true;
     Attributes[method] = _.contains(wrappedMethods, method) ? wrapper : fn;
   });
 
